1bb92a9268152325b1ef690c88ad84615ee4cec9
fabriciopimentel <fabriciopimentel87@gmail.com>
2026-06-04 16:52:59 -0300

    VALIDACIONES
    agrego funciones de validación :
    validación de números, validación de str

--- funciones.py
+++ funciones.py
@@ -1,7 +1,40 @@
 import questionary
 import os
+
 def limpiar_consola():
     os.system('cls' if os.name =='nt' else 'clear' )
+
+##funcion de validacion de str
+def validar_str(mensaje):
+    while True:
+        try:
+            nombre = input(mensaje).strip().lower()
+            
+            if nombre == "":
+                raise ValueError("no se puede ingresar vacío")
+            if not nombre.replace(" ", "").isalpha():
+                raise ValueError("solo letras")
+                
+            return nombre  
+            
+        except ValueError as e:
+            print(f"Error: {e}")
+
+
+##funcion de validacion de numeros
+def validar_numero(mensaje):
+    while True:
+        try:
+            num = int(input(mensaje))
+            
+            if num <= 0:
+                raise("error: debe ser mayor a 0")
+            return num
+        
+        except ValueError as e:
+            print(f"ERROR: {e}")
+
+
 def menu(paises):
     while True:
         opcion = questionary.select(
@@ -33,3 +66,4 @@ def menu(paises):
             case "Salir del programa":
                 print("Saliendo del programa...")
                 break
+
